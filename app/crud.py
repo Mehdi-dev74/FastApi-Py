d269cd8c3808a7fed24d806520380ac533243a1d
Mehdi <mehdi@gmail.com>
2026-05-14 22:24:27 +0200

    Implement CRUD API endpoints

--- app/crud.py
+++ app/crud.py
@@ -1,30 +1,25 @@
 from sqlmodel import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import User, Item
-from app.database import get_session
-from sqlalchemy.ext.asyncio import AsyncAttrs
 
-async def create_user(user: User) -> User:
-    async for session in get_session():
-        session.add(user)
-        await session.commit()
-        await session.refresh(user)
-        return user
+async def create_user(session: AsyncSession, user: User) -> User:
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return user
 
-async def get_user_by_username(username: str) -> User | None:
-    async for session in get_session():
-        result = await session.exec(select(User).where(User.username == username))
-        return result.first()
+async def get_user_by_username(session: AsyncSession, username: str) -> User | None:
+    result = await session.exec(select(User).where(User.username == username))
+    return result.first()
 
-async def create_item(item: Item) -> Item:
-    async for session in get_session():
-        session.add(item)
-        await session.commit()
-        await session.refresh(item)
-        return item
+async def create_item(session: AsyncSession, item: Item) -> Item:
+    session.add(item)
+    await session.commit()
+    await session.refresh(item)
+    return item
 
-async def get_items() -> list[Item]:
-    async for session in get_session():
-        result = await session.exec(select(Item))
-        return result.all()
+async def get_items(session: AsyncSession) -> list[Item]:
+    result = await session.exec(select(Item))
+    return result.all()
 
